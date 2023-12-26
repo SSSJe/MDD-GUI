3e70f3c37035759ea049880687a75f986067a639
SSSJec <62241145+SSSJe@users.noreply.github.com>
2023-12-26 15:35:50 +0800

    Update app.R

--- app.R
+++ app.R
@@ -1,3 +1,4 @@
+## Import R package
 library(RColorBrewer)
 library(ggsci)
 library(ggrepel)
@@ -9,66 +10,112 @@ library(DT)
 library(bs4Dash)
 library(ggplot2)
 
-mycolors <- unique(c(brewer.pal(name="Dark2", n = 8), 
-  brewer.pal(name="Accent", n = 7), brewer.pal(name="Set1", n = 9), 
-  brewer.pal(name="Set3", n = 12), brewer.pal(name="Set2", n = 8), 
-  brewer.pal(name="Paired", n = 5), brewer.pal(name="Paired", n = 10)[7:10], 
-  brewer.pal(name="Pastel1", n = 9), brewer.pal(name="Pastel2", n = 7)))
+## Set color
+mycolors <- unique(c(brewer.pal(name = "Dark2", n = 8), 
+  brewer.pal(name = "Accent", n = 7), brewer.pal(name = "Set1", n = 9), 
+  brewer.pal(name = "Set3", n = 12), brewer.pal(name = "Set2", n = 8), 
+  brewer.pal(name = "Paired", n = 5), brewer.pal(name = "Paired", n = 10)[7:10], 
+  brewer.pal(name = "Pastel1", n = 9), brewer.pal(name = "Pastel2", n = 7)))
 
+## Set user interface
 ui <- dashboardPage(
   dark = NULL,
   help = NULL,
   dashboardHeader(title = "MDD_GUI"),
-  dashboardSidebar(skin = "light", childIndent = TRUE, minified = FALSE, 
-     
+  dashboardSidebar(skin = "light", childIndent = TRUE, minified = FALSE,
+    
+    ## Set the input of cell majortype
     checkboxGroupInput(inputId = "check", label = "MajorType", 
               choices = list("Excitatory Neurons",
                 "Inhibitory Neurons",
                 "Oligodendrocytes",
                 "Astrocytes",
                 "Microglia/Macrophage",
-                "Endothelial"),selected = list("Excitatory Neurons","Inhibitory Neurons","Oligodendrocytes","Astrocytes","Microglia/Macrophage","Endothelial")), 
+                "Endothelial"),
+              selected = list("Excitatory Neurons", "Inhibitory Neurons", "Oligodendrocytes", "Astrocytes", "Microglia/Macrophage", "Endothelial")),
+    
+    ## Set the input of gene
     textInput("gene",'Search for A Gene',value = 'PCP4'),
-    column(12, submitButton('search',icon("search")), align = 'center', style = "margin-bottom: 10px;"
-        , style = "margin-top: -10px;")
+    column(12, submitButton('search',icon("search")), align = 'center', 
+      style = "margin-bottom: 10px;", style = "margin-top: -10px;")
   ),
 
+ ## Set the body of display content
   dashboardBody(
-
     tags$style(HTML(" 
         input:invalid { 
         background-color: #FFCCCC; 
         }")), 
+    
+    #### Set up shinyjs #### 
     useShinyjs(), 
+
+    ### shinyBS ### 
     bsAlert("alert"),
     textOutput('text_error'),
-
+    
+    ## Boxes need to be put in a row (or column)
     fluidRow(
+
+      ## Show the information of this website
       box(title = 'Info', id= "box_l",
         helpText('Website for Visualizing the snRNA-seq Profile of the Brodmann Area 9 (BA9) of the Post-Mortem Brain Tissue of 17 Control Subjects and 17 Major Depressive Disorder (MDD) Cases.',
           a('(See the paper more information. PMID: 32341540)', href = "https://doi.org/10.1038/s41593-020-0621-y",target="_blank")), 
         status = "info", width = 6),
+
+      ## Show the flow of this study      
       box(title = 'The flow of the study', id= "box_r", img(src = "MDD_flow.png",style = 'width: 100%;'), status = "info", align='center'),
       spsComps::heightMatcher("box_l","box_r")
       ),
-     fluidRow(
-      box(title = 'tSNE plot of MMD', id = 'box_t', status = "warning", 
+
+    fluidRow(
+      
+      ## Show the tSNE plot of cell majortypes 
+      box(title = 'tSNE plot of MMD majortypes', id = 'box_m', status = "warning", 
         solidHeader = TRUE, collapsible = FALSE,
         img(src = "MDD_Majortype.png", style = 'width: 90%;'), align='center'),
-      spsComps::heightMatcher("box_t", "box_v"),
-      box(title = 'tSNE plot of gene exp', id = 'box_v', status = "warning", 
-        solidHeader = TRUE, collapsible = FALSE,
-        plotOutput("tSNE_plot"), 
-        downloadButton('tSNE_download', 'Download'),width = 6),
 
-      box(title = 'Violin plot of gene exp', status = "primary", 
+      ## Show the tSNE plot of cell subtypes
+      box(title = 'tSNE plot of MMD subtypes', id = 'box_s', status = "warning", 
+        solidHeader = TRUE, collapsible = FALSE,
+        img(src = "MDD_subtype.png", style = 'width: 100%;'), align='center'),
+      spsComps::heightMatcher("box_m", "box_s"),
+      
+      ## Show the groups of cells
+      box(title = 'tSNE plot of MMD groups', id = 'box_t', status = "lightblue", 
+        solidHeader = TRUE, collapsible = FALSE,
+        img(src = "MDD_group.png", style = 'width: 100%;'), align='center'),
+
+      ## Location the tSNE plot of gene expression in cells
+      box(title = 'tSNE plot of gene expression', id = 'box_v', status = "lightblue", 
+        solidHeader = TRUE, collapsible = FALSE,
+        plotOutput("tSNE_plot"),
+        downloadButton('tSNE_download', 'Download'),width = 6),
+      spsComps::heightMatcher("box_t", "box_v"),
+      
+      ## Location the violin plot of gene expression in cells
+      box(title = 'Violin plot of gene expression', status = "primary", 
         solidHeader = TRUE, collapsible = FALSE,
         plotOutput("Violin_plot", height = 250), 
         downloadButton('Violin_download', 'Download'),width = 6),
-      box(title = 'Box plot of gene exp', status = "primary", solidHeader = TRUE, collapsible = FALSE,
-        plotOutput("Box_plot", height = 250),
-        downloadButton('Box_download', 'Download'),width = 6),
 
+      ## Location the box plot of gene expression in cells
+      box(title = 'Violin plot of gene expression between case and control', status = "primary", 
+        solidHeader = TRUE, collapsible = FALSE,
+        plotOutput("Violin_plot_cc", height = 250),
+        downloadButton('Violin_download_cc', 'Download'),width = 6),
+
+      ## Show mean of gene expression in cell majortypes
+      box(title = 'Summary of majortypes', status = 'success',
+        solidHeader = TRUE, collapsible = FALSE,
+        DTOutput("summary_m"), width = 6),
+      
+      ## Show mean of gene expression in cell subtypes
+      box(title = 'Summary of subtypes', status = 'success',
+        solidHeader = TRUE, collapsible = FALSE,
+        DTOutput("summary_s"), width = 6),
+     
+      ## Show the dataset of gene expression in cells
       box(title = 'DataTable',
         DTOutput("dataset"),
         downloadButton('dataset_download', 'Download'), width = 12)
@@ -77,8 +124,10 @@ ui <- dashboardPage(
 
 )
 
-server <- function(session,input, output) {
+## Set server process
+server <- function(session, input, output) {
   
+  ## Input the expression data and meta data
   datasetInput <- reactive({
     mat.meta <- readRDS('scMDD.obj.meta.all.rds')
     mat.exp <- readRDS('scMDD.obj.exp.all.rds')
@@ -94,13 +143,24 @@ server <- function(session,input, output) {
       return(as.matrix(0,nrow = nrow(mat.exp),ncol = 1))}
   })
 
+  ## data peocessing
+  datasetplt <- reactive({
+    dat.all <- datasetInput()
+    index.cell <- which(datasetInput()$MajorType %in% input$check)
+    dat.all$colours <- datasetInput()[,input$gene]
+    dat.plot <- dat.all[index.cell,]
+    return(dat.plot)
+    })
+
+  ## Display of error
   observeEvent(input$gene, { 
      if(!(input$gene %in% colnames(datasetInput()))) { 
       alert("ERROR: The input of 'Search for A Gene' may be incorrect or absent.") 
      } 
     })
   
-  output$text_error <-  renderText({
+  ## Display of error in bold
+  output$text_error <- renderText({
     if(!(input$gene %in% colnames(datasetInput()))) {
     shinyBS::createAlert(session, "alert", "myValueAlert", title = "Search for A Gene: Invalid input", style = "danger", content = "ERROR: The input of 'Search for A Gene' may be incorrect or absent.")
     }
@@ -108,16 +168,19 @@ server <- function(session,input, output) {
     shinyBS::closeAlert(session, "myValueAlert") 
     }
   })
-
+  
+  ## Show the tSNE plot of gene expression in cells
   output$tSNE_plot <- renderPlot({
     dat.all <- datasetInput()
     dat.all$colours <- datasetInput()[,input$gene]
     ggplot(dat.all, aes(tSNE_1, tSNE_2)) + geom_point(aes(color = colours)) + 
-    scale_colour_gradientn(colours = viridis:: plasma(100)) + 
-    labs(x = '', y = '', title = 'Scatter Plot',col = 'Gene Expression') +
-    theme_classic() + 
-    theme(plot.title = element_text(color = 'black', size = 15, face = 'bold', hjust = 0.5), axis.text = element_text(size = 12), legend.position = 'right')
+      scale_colour_gradientn(colours=viridis:: plasma(100)) + 
+      labs(x = '', y = '', title = 'Scatter Plot',col = 'Gene Expression') +
+      theme_classic() + 
+      theme(plot.title = element_text(color = 'black',size = 15,face = 'bold',hjust = 0.5), axis.text =element_text(size = 12), legend.position = 'right')
     })
+
+  ## Download the tSNE plot of gene expression in cells
   output$tSNE_download <- downloadHandler(
     filename = function() {
       paste0('tSNE plot of MMD.pdf')
@@ -127,18 +190,18 @@ server <- function(session,input, output) {
       dat.all$colours <- datasetInput()[,input$gene]
       pdf(file, width = 6, height = 5)
       p1 <- ggplot(dat.all, aes(tSNE_1, tSNE_2)) + geom_point(aes(color = colours)) + 
-        scale_colour_gradientn(colours = viridis:: plasma(100)) + 
+        scale_colour_gradientn(colours=viridis:: plasma(100)) + 
         labs(x = '', y = '', title = 'Scatter Plot',col = 'Gene Expression') +
         theme_classic() + 
-        theme(plot.title = element_text(color = 'black', size = 15, face = 'bold', hjust = 0.5), axis.text = element_text(size = 12), legend.position = 'right')
+        theme(plot.title = element_text(color = 'black',size = 15,face = 'bold',hjust = 0.5), axis.text =element_text(size = 12), legend.position = 'right')
       print(p1)
       dev.off()
-    },contentType ="image/png"
+    }, contentType ="image/png"
   )
 
+  ## Show the violin plot of gene expression in cells
   output$Violin_plot <- renderPlot({
-    dat.all <- datasetInput()[which(datasetInput()$MajorType %in% input$check),c('subtype','MajorType',input$gene)]
-    dat.all$colours <- dat.all[,input$gene]
+    dat.all <- datasetplt()
     if (length(input$check) == 1) {
         dat.all$plot_type <- dat.all[,'subtype']
       }
@@ -149,16 +212,16 @@ server <- function(session,input, output) {
       labs(x = '', y = '', title = 'Violin plot') +
       scale_fill_manual(values = mycolors) +
       theme_classic() + 
-      theme(plot.title = element_text(color = 'black', size = 15, face = 'bold', hjust = 0.5), axis.text = element_text(size = 12), axis.text.x = element_text(angle = 45, hjust = 1, vjust =1), legend.position = 'none')
-    
+      theme(plot.title = element_text(color = 'black',size = 15,face = 'bold',hjust = 0.5), axis.text =element_text(size = 12), axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),legend.position = 'none')
   })
+
+  ## Download the violin plot of gene expression in cells
   output$Violin_download <- downloadHandler(
     filename = function() {
       paste0('cluster_Vioplot', '_', input$gene, '.pdf', sep = '')
     },
     content = function(file) {
-      dat.all <- datasetInput()[which(datasetInput()$MajorType %in% input$check),c('subtype','MajorType',input$gene)]
-      dat.all$colours <- dat.all[,input$gene]
+      dat.all <- datasetplt()
       if (length(input$check) == 1) {
         dat.all$plot_type <- dat.all[,'subtype']
       }
@@ -170,35 +233,35 @@ server <- function(session,input, output) {
         labs(x = '', y = '', title = 'Violin plot') +
         scale_fill_manual(values = mycolors) +
         theme_classic() + 
-        theme(plot.title = element_text(color = 'black', size = 15, face = 'bold', hjust = 0.5), axis.text = element_text(size = 12), axis.text.x = element_text(angle = 45, hjust = 1, vjust =1), legend.position = 'none')
+        theme(plot.title = element_text(color = 'black',size = 15,face = 'bold',hjust = 0.5), axis.text =element_text(size = 12), axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),legend.position = 'none')
       print(p2)
       dev.off()
     }
   )
 
-    output$Box_plot <- renderPlot({
-      dat.all <- datasetInput()[which(datasetInput()$MajorType %in% input$check),c('subtype','MajorType', input$gene)]
-      dat.all$colours <- dat.all[,input$gene]
-      if (length(input$check) == 1) {
-        dat.all$plot_type <- dat.all[,'subtype']
-      }
-      else{
-        dat.all$plot_type <- dat.all[,'MajorType']
-      }
-      ggplot(dat.all) + geom_boxplot(aes(plot_type, colours, color = plot_type)) + 
-        labs(x = '', y = '', title = 'Boxplot') +
-        scale_fill_manual(values = mycolors) +
-        theme_classic() + 
-        theme(plot.title = element_text(color = 'black', size = 15, face = 'bold', hjust = 0.5), axis.text = element_text(size = 12), axis.text.x = element_text(angle = 45, hjust = 1, vjust =1), legend.position = 'none')
+  ## Show the box plot of gene expression in cells
+  output$Violin_plot_cc <- renderPlot({
+    dat.all <- datasetplt()
+    if (length(input$check) == 1) {
+      dat.all$plot_type <- dat.all[,'subtype']
+    }
+    else{
+      dat.all$plot_type <- dat.all[,'MajorType']
+    }
+    ggplot(dat.all) + geom_violin(aes(plot_type, colours, color = Group)) + 
+      labs(x = '', y = '', title = 'Violin plot') +
+      scale_fill_manual(values = mycolors) +
+      theme_classic() + 
+      theme(plot.title = element_text(color = 'black',size = 15,face = 'bold',hjust = 0.5), axis.text =element_text(size = 12), axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),legend.position = 'right')
   })
 
-  output$Box_download <- downloadHandler(
+  ## Download the box plot of gene expression in cells
+  output$Violin_download_cc <- downloadHandler(
     filename = function() {
-      paste0('cluster_Boxplot', '_', input$gene, '.pdf', sep = '')
+      paste0('case_control_violinplot', '_', input$gene, '.pdf', sep = '')
     },
     content = function(file) {
-      dat.all <- datasetInput()[which(datasetInput()$MajorType %in% input$check),c('subtype','MajorType',input$gene)]
-      dat.all$colours <- dat.all[,input$gene]
+      dat.all <- datasetplt()
       if (length(input$check) == 1) {
         dat.all$plot_type <- dat.all[,'subtype']
       }
@@ -206,28 +269,49 @@ server <- function(session,input, output) {
         dat.all$plot_type <- dat.all[,'MajorType']
       }
       pdf(file, width = 6, height = 5)
-      p3 <- ggplot(dat.all) + geom_boxplot(aes(plot_type, colours, color = plot_type)) + 
-        labs(x = '', y = '', title = 'Boxplot') +
+      p3 <- ggplot(dat.all) + geom_violin(aes(plot_type, colours, color = Group)) + 
+        labs(x = '', y = '', title = 'Violin plot') +
         scale_fill_manual(values = mycolors) +
         theme_classic() + 
-        theme(plot.title = element_text(color = 'black', size = 15, face = 'bold', hjust = 0.5), axis.text = element_text(size = 12), axis.text.x = element_text(angle = 45, hjust = 1, vjust =1), legend.position = 'none')
+        theme(plot.title = element_text(color = 'black',size = 15,face = 'bold',hjust = 0.5), axis.text =element_text(size = 12), axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),legend.position = 'right')
       print(p3)
       dev.off()
     }
   )
-
-  output$dataset <- renderDT({
-    data.set <- datasetInput()[which(datasetInput()$MajorType %in% input$check),c('Clusters','MajorType','subtype',input$gene)]
+  ## Calculate the expression of majortypes
+  output$summary_m <- renderDT({
+    index.m <- which(datasetInput()$MajorType %in% input$check)
+    data.sub <- datasetInput()[index.m,]
+    data.set <- aggregate(data.sub[,7], list(data.sub$MajorType), mean)
+    datatable(data.set, colnames = c('MajorType', 'Mean'), options = list(pageLength = 6))
     })
+
+  ## Calculate the expression of subtypes
+     output$summary_s <- renderDT({
+      index.m <- which(datasetInput()$MajorType %in% input$check)
+      data.sub <- datasetInput()[index.m,]
+      data.set <- aggregate(data.sub[,7] ~ subtype, data.sub, mean)
+      datatable(data.set, colnames = c('Subtype', 'Mean'), options = list(pageLength = 6))
+    })
+
+  ## View the expression of gene in each cells
+  output$dataset <- renderDT({
+    index.m <- which(datasetInput()$MajorType %in% input$check)
+    data.set <- datasetInput()[index.m, c('Clusters', 'MajorType', 'subtype', input$gene)]
+    })
+
+  ## Download the expression of gene in each cells
   output$dataset_download <- downloadHandler(
     filename = function() {
       paste0('Exp_MMD', input$gene, '_.csv', sep='')
     },
     content = function(file) {
-      data.set <- datasetInput()[which(datasetInput()$MajorType %in% input$check),c('Clusters','MajorType','subtype',input$gene)]
+      index.m <- which(datasetInput()$MajorType %in% input$check)
+      data.set <- datasetInput()[index.m, c('Group', 'Clusters', 'MajorType', 'subtype', input$gene)]
       write.csv(data.set, file)
     }
   )
 }
 
+## Run shinyApp
 shinyApp(ui, server,option = list(launch.browser = TRUE))
